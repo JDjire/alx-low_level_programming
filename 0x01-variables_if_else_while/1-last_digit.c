7fcf96317295fc2dd4d944932007b658275327e2
JDjire <jiregnadereje35@gmail.com>
2022-12-09 11:14:37 -0800

    variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-##include <stdlib.h>
-##include <time.h>
-#/**
+#include <stdlib.h>
+#include <time.h>
+/**
  * main - Entry point
  * Description: checks on the last digit of the assigned var
  * Return: Always 0 (success)
