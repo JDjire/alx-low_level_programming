a87c6e962892b4e16616b2d336a124b54e649ff7
JDjire <jiregnadereje35@gmail.com>
2022-12-13 07:41:45 -0800

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,31 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 52 fibonacci numbers
+ * main - Prints natural numbers below 1024 that are
+ * multiplies of 3 or 5
  *
- * Return: Nothing!
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2;
+        int a, b;
 
-	while (i < 50)
+        for (a = 1; a < 1024; a++)
 	{
-		if (i == 0)
-			printf("%ld", j);
-		else if (i == 1)
-			printf(", %ld", k);
-		else
-		{
-			k += j;
-			j = k - j;
-			printf(", %ld", k);
-		}
-
-		++i;
+	        if ((a % 3) == 0 || (a % 5) == 0)
+		        b += a;
 	}
-
-	printf("\n");
-	return (0);
-}
+        printf("%d\n", b);
+        return (0);
+}	
